b7ac97f416089b2a095f6a2cc1ddd79b3634edf7
megaseves <megaseves@gmail.com>
2023-03-02 16:56:33 +0100

    Add: change some basic design element on Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,11 +7,11 @@ export function Home() {
         <div className="home-container">
             <div className="greeting">
                 <img src={process.env.PUBLIC_URL + '/burger.png'} alt="" className="greeting-image"/>
-                <p className={"text"}>Order Tasty &<br/>Fresh Food<br/><span className={"color-sec"}>anytime!</span></p>
-                <p className="reserve-text">Reserve a table by calling <a href="tel:303333333" className={"color-sec bold"}>30 3333 333</a><br/>or order at home!</p>
+                <p className={"text"}><span className={"big-font"}>ORDER<br className={"line-break"} />  TASTY <br /> <span className={"and-line-top"}></span><i><p className={"and"}>and</p></i><span className={"and-line-bottom"}></span></span><br/>FRESH FOOD <span className={"color-sec"}>ANYTIME!</span></p>
+                <p className="reserve-text">Reserve a table by calling <a href="tel:303333333" className={"color-sec bold"}>30 3333 333</a></p>
                 <div className="order-and-menu">
-                    <div className="order-button">Order Now</div>
-                    <p className="menu-button color-sec">See Menu</p>
+                    <div className="order-button">See Menu</div>
+                    <p className="menu-button color-sec">See Location</p>
                 </div>
             </div>
         </div>
